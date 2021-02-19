9fd9b7d689cac27eb05f9bbceb49d591acef767f
Willian Oliveira <wijloc@gmail.com>
2021-02-19 18:02:30 -0300

    feat: list items of cart

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,7 +30,12 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storagedProducts = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+      if (storagedProducts) {
+        setProducts([...JSON.parse(storagedProducts)]);
+      }
     }
 
     loadProducts();
